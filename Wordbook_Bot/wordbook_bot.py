5880b43a95be6a93ec82c326f32f139f17e8df5f
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-04-27 17:13:51 +0200

    d

--- Wordbook_Bot/wordbook_bot.py
+++ Wordbook_Bot/wordbook_bot.py
@@ -10,14 +10,27 @@ import random
 
 def run_bot(r):
     try:
-        subreddit = r.subreddit("Shinobunfriends")
+        marvin_quotes = \
+        [
+        "It's not good to expect too much, but you can't do anything if you're being overly pessimistic. If you just wait thinking it's useless, nothing will come of it.",
+        "No matter what bonds you forge with others, time will tear them apart. Well... Doesn't thinking about it make you sick?",
+        "It's difficult to change the world on your own, but twisting it a little might not be all that hard.",
+        "There's no reason a fake can't do what the real thing would. And it's possible for a fake to be more real than the real thing.",
+        "The sun is my enemy, but the moon has been good to me.",
+        "We can't let the past be mere water under the bridge. Even so, there's no reason that we can't come together."
+        ]
+
+        
+
+        subreddit = r.subreddit("DuckGameClips")
+
         for comment in subreddit.stream.comments():
             print(comment.body)
-            if re.search("Shinobu Order", comment.body, re.IGNORECASE):
-                    marvin_reply = "[Suggested watch order](https://media.discordapp.net/attachments/652432414135681060/662034140505571378/6gqy1AQaz0AXwlkBaVPMP-ST8fwleVWMLFXAcWkBHOM.png?width=617&height=904)"
+            if re.search("Shinobu Help", comment.body, re.IGNORECASE):
+                    marvin_reply = "Shinobu says: " + random.choice(marvin_quotes)
                     comment.reply(marvin_reply)
                     print(marvin_reply)
-    
+         
     # Low karma
     except Exception as e:
         time_remaining = 15
