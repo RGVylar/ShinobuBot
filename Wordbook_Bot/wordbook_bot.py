3822744af02f228b67fe3d4ae54356998c24aeb2
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-04-27 16:07:14 +0200

    Update wordbook_bot.py

--- Wordbook_Bot/wordbook_bot.py
+++ Wordbook_Bot/wordbook_bot.py
@@ -9,9 +9,5 @@ import re
 
 if __name__ == "__main__":
     print ("\nBot on...")
-        try:
-            r = bot_login.bot_login()
-            print ("\nBot log")
-        except Exception as e:
-            print (str(e.__class__.__name__) + ": " + str(e))
-            time.sleep(15)
+    r = bot_login.bot_login()
+    print ("\nBot log")
