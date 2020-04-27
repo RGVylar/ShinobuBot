4abccb8cbb45ae90ca0495669a63cca54506abb8
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-04-27 16:05:41 +0200

    Update wordbook_bot.py

--- Wordbook_Bot/wordbook_bot.py
+++ Wordbook_Bot/wordbook_bot.py
@@ -8,8 +8,10 @@ import psycopg2
 import re
 
 if __name__ == "__main__":
+    print ("\nBot on...")
         try:
             r = bot_login.bot_login()
+            print ("\nBot log")
         except Exception as e:
             print (str(e.__class__.__name__) + ": " + str(e))
             time.sleep(15)
