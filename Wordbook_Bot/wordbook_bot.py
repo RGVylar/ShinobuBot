3611583a843e0abc15324fb7fcdc170f25073b73
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-04-27 16:10:01 +0200

    Update wordbook_bot.py

--- Wordbook_Bot/wordbook_bot.py
+++ Wordbook_Bot/wordbook_bot.py
@@ -11,3 +11,24 @@ if __name__ == "__main__":
     print ("\nBot on...")
     r = bot_login.bot_login()
     print ("\nBot log")
+    marvin_quotes = \
+    [
+    "It's not good to expect too much, but you can't do anything if you're being overly pessimistic. If you just wait thinking it's useless, nothing will come of it.",
+    "No matter what bonds you forge with others, time will tear them apart. Well... Doesn't thinking about it make you sick?",
+    "It's difficult to change the world on your own, but twisting it a little might not be all that hard.",
+    "There's no reason a fake can't do what the real thing would. And it's possible for a fake to be more real than the real thing.",
+    "The sun is my enemy, but the moon has been good to me.",
+    "We can't let the past be mere water under the bridge. Even so, there's no reason that we can't come together."
+    ]
+
+    reddit = praw.Reddit('bot1')
+
+    subreddit = reddit.subreddit("DuckGameClips")
+
+    for comment in subreddit.stream.comments():
+        print(comment.body)
+        if re.search("Shinobu Help", comment.body, re.IGNORECASE):
+                marvin_reply = "Shinobu says: " + random.choice(marvin_quotes)
+                comment.reply(marvin_reply)
+                print(marvin_reply)
+                
