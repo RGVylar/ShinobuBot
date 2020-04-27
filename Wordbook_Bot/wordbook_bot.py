585c78cf8f980c176aa02847b158972885aa9e9d
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-04-27 16:02:14 +0200

    Update wordbook_bot.py

--- Wordbook_Bot/wordbook_bot.py
+++ Wordbook_Bot/wordbook_bot.py
@@ -11,17 +11,6 @@ if __name__ == "__main__":
     while True:
         try:
             r = bot_login.bot_login()
-
-            
-
-            
-
-            print ("\nFetching comments..")
-            while True:
-                # Fetching all new comments that were created after created_utc time
-                
-                time.sleep(10)
-
         except Exception as e:
             print (str(e.__class__.__name__) + ": " + str(e))
             time.sleep(15)
